997aa2bc63919f7e7ea69f7e497ba84c484de6e1
Gustavo Costa <168137183+gustavo123c@users.noreply.github.com>
2025-05-07 20:02:23 -0300

    Update TesouroGema.java

--- TesouroGema.java
+++ TesouroGema.java
@@ -1,1 +1,11 @@
 
+public class TesouroGema extends Tesouro {
+    public TesouroGema(String nome, int[] localizacao, int valor) {
+        super(nome, localizacao, valor * 2); // Gemas valem o dobro
+    }
+
+    @Override
+    public void efeito() {
+        System.out.println("Gema preciosa coletada! +" + getValor() + " pontos");
+    }
+}
