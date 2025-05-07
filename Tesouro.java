83f8e62c714122a17e73b18b93c1e0bb2b8deb85
Gustavo Costa <168137183+gustavo123c@users.noreply.github.com>
2025-05-07 20:02:55 -0300

    Update Tesouro.java

--- Tesouro.java
+++ Tesouro.java
@@ -12,6 +12,7 @@ public abstract class Tesouro {
     public String getNome() { 
         return nome; 
     }
+    
     public void setNome(String nome) { 
         this.nome = nome; 
     }
@@ -19,6 +20,7 @@ public abstract class Tesouro {
     public int[] getLocalizacao() { 
         return localizacao; 
     }
+    
     public void setLocalizacao(int[] localizacao) { 
         this.localizacao = localizacao; 
     }
@@ -26,31 +28,10 @@ public abstract class Tesouro {
     public int getValor() { 
         return valor; 
     }
+    
     public void setValor(int valor) { 
         this.valor = valor; 
     }
 
     public abstract void efeito();
-}
-
-public class TesouroOuro extends Tesouro {
-    public TesouroOuro(String nome, int[] localizacao, int valor) {
-        super(nome, localizacao, valor);
-    }
-
-    @Override
-    public void efeito() {
-        System.out.println("Ouro coletado! +" + getValor() + " pontos");
-    }
-}
-
-public class TesouroGema extends Tesouro {
-    public TesouroGema(String nome, int[] localizacao, int valor) {
-        super(nome, localizacao, valor * 2); // Gemas valem o dobro
-    }
-
-    @Override
-    public void efeito() {
-        System.out.println("Gema preciosa coletada! +" + getValor() + " pontos");
-    }
 }
