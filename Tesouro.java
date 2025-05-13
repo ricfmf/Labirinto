b1885bdee2ee5628c258eb2f28cb2cbdb0005b9b
ricfmf <75329136+ricfmf@users.noreply.github.com>
2025-05-13 17:31:49 -0300

    Update Tesouro.java

--- Tesouro.java
+++ Tesouro.java
@@ -1,33 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.projeto2gq;
 
 /**
- *
- * @author ricar
+ * Representa um artefato digital valioso no sistema Tron.
  */
-
-import java.util.Arrays;
-
 public abstract class Tesouro {
     private String nome;
-    private int[] localizacao;
     private int valor;
+    private int[] localizacao;
 
-    public Tesouro(String nome, int[] localizacao, int valor) {
+    public Tesouro(String nome, int valor, int[] localizacao) {
         this.nome = nome;
-        this.localizacao = localizacao;
         this.valor = valor;
+        this.localizacao = localizacao;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public int getValor() {
+        return valor;
     }
 
     public int[] getLocalizacao() {
@@ -38,39 +30,8 @@ public abstract class Tesouro {
         this.localizacao = localizacao;
     }
 
-    public int getValor() {
-        return valor;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
-
-    public boolean estaNaPosicao(int[] posicao) {
-        return Arrays.equals(this.localizacao, posicao);
-    }
-
+    /**
+     * Executa um efeito especial quando o tesouro é coletado.
+     */
     public abstract void efeito();
-}
-
-class TesouroOuro extends Tesouro {
-    public TesouroOuro(String nome, int[] localizacao, int valor) {
-        super(nome, localizacao, valor);
-    }
-
-    @Override
-    public void efeito() {
-        System.out.println("Ouro coletado! +" + getValor() + " pontos");
-    }
-}
-
-class TesouroGema extends Tesouro {
-    public TesouroGema(String nome, int[] localizacao, int valorBase) {
-        super(nome, localizacao, valorBase * 2); // Gema vale o dobro
-    }
-
-    @Override
-    public void efeito() {
-        System.out.println("Gema preciosa coletada! +" + getValor() + " pontos");
-    }
 }
